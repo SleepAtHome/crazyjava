4a2f59c9eb82f55ecf6959b30874f307f7dcfd29
JYK <773669388@qq.com>
2019-11-04 22:37:29 +0800

    集合1

---
+++ src/main/java/Collection/CollectionTest.java
@@ -0,0 +1,43 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        Collection c = new ArrayList();
+
+        // 增加元素
+        c.add("景元奎");
+        c.add(6); // 会自动装箱
+        System.out.println(c.size());
+
+        // 删除指定元素
+        c.remove(6);
+        System.out.println(c.size());
+
+        // 判断是否包含某个元素
+        System.out.println(c.contains("景元奎"));
+        c.add("BOOK1");
+        System.out.println(c);
+
+        // 判断是否包含某个集合
+        Collection books = new HashSet();
+        books.add("BOOK1");
+        books.add("BOOK2");
+        System.out.println(c.containsAll(books));
+
+        // 删除c集合中包含books集合的所有元素
+        c.removeAll(books);
+        System.out.println(c);
+
+        // 删除集合中所有的元素
+        c.clear();
+        System.out.println(c);
+
+        // 取交集
+        books.retainAll(c);
+        System.out.println(books);
+    }
+}
---
+++ src/main/java/Collection/IteratorTest.java
@@ -0,0 +1,25 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class IteratorTest {
+    public static void main(String[] args) {
+        Collection books = new ArrayList();
+        books.add("BOOK1");
+        books.add("BOOK2");
+        books.add("BOOK3");
+
+        // 通过集合获取迭代器
+        Iterator it = books.iterator();
+
+        // 当集合中有下一个元素的时候
+        while (it.hasNext()) {
+            // it.next() 指向集合中的下一个元素，并且返回当前元素
+            String book = (String) it.next();
+            System.out.println(book);
+        }
+    }
+}
