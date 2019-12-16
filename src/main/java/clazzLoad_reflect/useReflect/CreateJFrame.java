c2b182ffe277a541f41817421145bf81470afe77
JYK <773669388@qq.com>
2019-12-16 23:53:30 +0800

    18.4.1 使用反射创建对象 Finished 优化

--- src/main/java/clazzLoad_reflect/useReflect/CreateJFrame.java
+++ src/main/java/clazzLoad_reflect/useReflect/CreateJFrame.java
@@ -14,7 +14,9 @@ import java.lang.reflect.InvocationTargetException;
 public class CreateJFrame {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Class<?> jframeClazz = Class.forName("javax.swing.JFrame");
+        // 获取JFrame中带一个字符串参数的构造器
         Constructor constructor = jframeClazz.getConstructor(String.class);
+        // 使用指定构造器创建对象
         Object obj = constructor.newInstance("测试窗口");
         System.out.println(obj);
     }
